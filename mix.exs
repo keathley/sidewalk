5c177aa520d47f0b83340857ba90a026b2b59390
Matthias Kalb <matthias.kalb@railsmechanic.de>
2017-01-27 08:41:32 +0100

    Minor fixes and updates
    - Updated dependencies
    - Fixed Elixir 1.4.0 warnings

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sidewalk.Mixfile do
   def project do
     [
       app: :sidewalk,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -21,11 +21,11 @@ defmodule Sidewalk.Mixfile do
 
   defp deps do
     [
-      {:redix, "~> 0.4.0"},
-      {:poolboy, "~> 1.5"},
-      {:poison, "~> 3.0"},
-      {:ex_doc, "~> 0.14.3", only: [:dev]},
-      {:earmark, "~> 1.0.2", only: [:dev]}
+      {:redix,    "~> 0.5.1"},
+      {:poolboy,  "~> 1.5.1"},
+      {:poison,   "~> 2.2.0"},
+      {:ex_doc,   "~> 0.14.5",  only: [:dev]},
+      {:earmark,  "~> 1.1.0",   only: [:dev]}
     ]
   end
 
